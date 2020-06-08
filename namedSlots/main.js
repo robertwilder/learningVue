509e7495ad81aceac3d971c0c2102f66fc840711
Robert Wilder <wilder@macbook-pro.home>
2020-06-08 11:03:00 +0100

    named slots

---
+++ namedSlots/main.js
@@ -0,0 +1,36 @@
+
+
+Vue.component('modal', {
+    template: `
+    <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">
+        
+        <slot name="header"></slot>
+        
+        </p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+
+        <slot></slot>
+
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success">Save changes</button>
+        <button class="button">Cancel</button>
+      </footer>
+    </div>
+  </div>
+  
+    `
+}),
+
+
+new Vue({
+    el: '#root'
+});
+
+Vue.config.devtools = true;
